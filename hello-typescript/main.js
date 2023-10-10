6a2d01d9a6b533ffef6f3719a91f085c75aff3fb
Ely Andrade <126113145+ElyjAndrade@users.noreply.github.com>
2023-10-10 09:14:12 -0300

    .

--- hello-typescript/main.js
+++ hello-typescript/main.js
@@ -4,8 +4,18 @@ function minhaFunc(x, y) {
 }
 // ecmascript javascript
 var num = 2;
-var PI = 3.14;
+var PI = 3.14; 
 var numeros = [1, 2, 3];
 numeros.map(function (valor) {
     return valor * 2;
 });
+numeros.map(function (valor) { return valor * 2; });
+// a função acima foi simplificada com o uso de arrow function.
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
--- hello-typescript/main.ts
+++ hello-typescript/main.ts
@@ -9,9 +9,15 @@ let num = 2;
 const PI = 3.14;
 
 var numeros = [1, 2, 3];
-numeros.map(function(valor){
+numeros.map(function(valor){            
     return valor * 2;
-});
+});        
+numeros.map(valor => valor * 2);       
+// a função acima foi simplificada com o uso de arrow function.
 
-
+class Matematica {
+    soma(x, y){
+        return x + y;
+    }
+}
 
