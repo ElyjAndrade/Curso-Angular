461f26d534d134de742d223dd0cb1ad58e2956f1
Ely Andrade <126113145+ElyjAndrade@users.noreply.github.com>
2023-10-10 07:41:48 -0300

    .

---
+++ hello-typescript/main.ts
@@ -0,0 +1,17 @@
+var minhaVar = 'minha variavel'; //declaração em JavaScript.
+
+function minhaFunc(x, y) {
+    return x + y;
+}
+
+// ecmascript javascript
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(function(valor){
+    return valor * 2;
+});
+
+
+
